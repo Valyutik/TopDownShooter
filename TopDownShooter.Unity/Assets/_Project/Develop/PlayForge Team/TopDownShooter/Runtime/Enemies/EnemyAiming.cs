9edc283f5b2d6b651c0943b7a832378350fa0a00
Sergei Lind <lind-2004@mail.ru>
2024-02-19 23:03:43 +0300

    Add the ability to shoot enemies

---
+++ TopDownShooter.Unity/Assets/_Project/Develop/PlayForge Team/TopDownShooter/Runtime/Enemies/EnemyAiming.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using PlayForge_Team.TopDownShooter.Runtime.Characters;
+using PlayForge_Team.TopDownShooter.Runtime.Players;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+namespace PlayForge_Team.TopDownShooter.Runtime.Enemies
+{
+    public sealed class EnemyAiming : CharacterAiming
+    {
+        [SerializeField] private float aimingSpeed = 10f;
+        [SerializeField] private Vector3 aimDeltaPosition = Vector3.up;
+        [SerializeField] private float aimingRange = 20f;
+        
+        private Transform _aimTransform;
+        private RigBuilder _rigBuilder;
+        private WeaponAiming[] _weaponAimings;
+        private Transform _targetTransform;
+        private bool _isTargetInRange;
+        
+        protected override void OnInit()
+        {
+            _aimTransform = CreateAim().transform;
+            _rigBuilder = GetComponentInChildren<RigBuilder>();
+            _weaponAimings = GetComponentsInChildren<WeaponAiming>(true);
+
+            _targetTransform = FindAnyObjectByType<Player>().transform;
+            SetDefaultAimPosition();
+            InitWeaponAimings(_weaponAimings, _aimTransform);
+        }
+        
+        private void InitWeaponAimings(IReadOnlyList<WeaponAiming> weaponAimings, Transform aim)
+        {
+            foreach (var weaponAiming in weaponAimings)
+            {
+                weaponAiming.Init(aim);
+            }
+            _rigBuilder.Build();
+        }
+        
+        private void Update()
+        {
+            if (!IsActive)
+            {
+                return;
+            }
+            Aiming();
+        }
+
+        private void Aiming()
+        {
+            if (CheckTargetInRange())
+            {
+                _isTargetInRange = true;
+                _aimTransform.position = Vector3.Lerp(_aimTransform.position,
+                    _targetTransform.position + aimDeltaPosition, aimingSpeed * Time.deltaTime);
+            }
+            else
+            {
+                if (_isTargetInRange)
+                {
+                    _isTargetInRange = false;
+                    SetDefaultAimPosition();
+                }
+            }
+            
+            var lookDirection = (_aimTransform.position - transform.position).normalized;
+            lookDirection.y = 0;
+            var newRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, aimingSpeed * Time.deltaTime);
+        }
+        
+        private bool CheckTargetInRange()
+        {
+            return (_targetTransform.position - transform.position).magnitude <= aimingRange;
+        }
+        
+        private GameObject CreateAim()
+        {
+            var aim = new GameObject("EnemyAim");
+            aim.transform.SetParent(transform);
+            return aim;
+        }
+        
+        private void SetDefaultAimPosition()
+        {
+            var tr = transform;
+            _aimTransform.position = tr.position + tr.forward + aimDeltaPosition;
+        }
+    }
+}
---
+++ TopDownShooter.Unity/Assets/_Project/Develop/PlayForge Team/TopDownShooter/Runtime/Enemies/EnemyShooting.cs
@@ -0,0 +1,83 @@
+﻿using PlayForge_Team.TopDownShooter.Runtime.Bullets;
+using PlayForge_Team.TopDownShooter.Runtime.Characters;
+using PlayForge_Team.TopDownShooter.Runtime.Players;
+using UnityEngine;
+
+namespace PlayForge_Team.TopDownShooter.Runtime.Enemies
+{
+    public sealed class EnemyShooting : CharacterShooting
+    {
+        [SerializeField] private Bullet bulletPrefab;
+        [SerializeField] private float bulletDelay = 0.05f;
+        [SerializeField] private float shootingRange = 10f;
+        [SerializeField] private int bulletsInRow = 7;
+        [SerializeField] private float reloadingDuration = 4f;
+        private Transform _bulletSpawnPoint;
+        private Transform _targetTransform;
+        private float _bulletTimer;
+        private int _currentBulletsInRow;
+        
+        protected override void OnInit()
+        {
+            _bulletSpawnPoint = GetComponentInChildren<BulletSpawnPoint>().transform;
+            _targetTransform = FindAnyObjectByType<Player>().transform;
+            Reload();
+        }
+
+        private void Reload()
+        {
+            _bulletTimer = 0;
+            _currentBulletsInRow = bulletsInRow;
+        }
+        
+        private void Update()
+        {
+            if (!IsActive)
+            {
+                return;
+            }
+            
+            _bulletTimer += Time.deltaTime;
+            Shooting();
+            Reloading();
+        }
+
+        private void Shooting()
+        {
+            if (CheckTargetInRange() && CheckHasBulletsInRow() && _bulletTimer >= bulletDelay)
+            {
+                Shoot();
+            }
+        }
+
+        private void Reloading()
+        {
+            if (!CheckHasBulletsInRow() && _bulletTimer >= reloadingDuration)
+            {
+                Reload();
+            }
+        }
+        
+        private bool CheckTargetInRange()
+        {
+            return (_targetTransform.position - transform.position).magnitude <= shootingRange;
+        }
+
+        private bool CheckHasBulletsInRow()
+        {
+            return _currentBulletsInRow > 0;
+        }
+        
+        private void Shoot()
+        {
+            _bulletTimer = 0;
+            SpawnBullet();
+            _currentBulletsInRow--;
+        }
+
+        private void SpawnBullet()
+        {
+            Instantiate(bulletPrefab, _bulletSpawnPoint.position, _bulletSpawnPoint.rotation);
+        }
+    }
+}
