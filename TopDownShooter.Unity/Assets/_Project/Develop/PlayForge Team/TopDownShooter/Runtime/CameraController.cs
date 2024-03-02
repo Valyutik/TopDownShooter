e05813ffbbfb8980746d238fd7380fb2a8b0676f
Sergei Lind <lind-2004@mail.ru>
2024-03-02 18:46:49 +0300

    Fix the camera zoom

--- TopDownShooter.Unity/Assets/_Project/Develop/PlayForge Team/TopDownShooter/Runtime/CameraController.cs
+++ TopDownShooter.Unity/Assets/_Project/Develop/PlayForge Team/TopDownShooter/Runtime/CameraController.cs
@@ -1,19 +1,21 @@
 ﻿using PlayForge_Team.TopDownShooter.Runtime.Players;
+using Cinemachine;
 using UnityEngine;
 
 namespace PlayForge_Team.TopDownShooter.Runtime
 {
     public sealed class CameraController : MonoBehaviour
     {
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
         [SerializeField] private PlayerAction playerAction;
-        [SerializeField] private Transform target;
         [SerializeField] private Transform cameraRoot;
         [SerializeField] private Transform cameraTransform;
         [SerializeField] private float rotationSpeed = 65f;
         [SerializeField] private float zoomSpeed = 10f;
         [SerializeField] private float minZoom = 3f;
         [SerializeField] private float maxZoom = 14f;
-        private float _currentZoom;
+        
+        private CinemachineFramingTransposer _transposer;
         
         private void Start()
         {
@@ -22,7 +24,7 @@ namespace PlayForge_Team.TopDownShooter.Runtime
 
         private void Init()
         {
-            _currentZoom = (target.position - cameraTransform.position).magnitude;
+            _transposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
         }
         
         private void LateUpdate()
@@ -60,16 +62,14 @@ namespace PlayForge_Team.TopDownShooter.Runtime
             }
             
             var direction = playerAction.ZoomCamera;
-            
             if (Mathf.Approximately(direction, 0))
             {
                 return;
             }
-            _currentZoom += direction * zoomSpeed * Time.deltaTime;
+            var currentZoom = direction * zoomSpeed * Time.deltaTime;
 
-            _currentZoom = Mathf.Clamp(_currentZoom, minZoom, maxZoom);
-
-            cameraTransform.position = cameraRoot.position - cameraTransform.forward * _currentZoom;
+            _transposer.m_CameraDistance += currentZoom;
+            _transposer.m_CameraDistance = Mathf.Clamp(_transposer.m_CameraDistance, minZoom, maxZoom);
         }
     }
 }
