1cbf270e2961ab9a8dec044aa95a78c76ad0297a
Sergey Lind <lind-2004@mail.ru>
2024-03-16 16:17:12 +0300

    Add loading, win, lose and current level display screens

---
+++ TopDownShooter.Unity/Assets/_Project/Develop/PlayForge Team/TopDownShooter/Runtime/UI/Screens/LoadingScreen.cs
@@ -0,0 +1,7 @@
+﻿namespace PlayForge_Team.TopDownShooter.Runtime.UI.Screens
+{
+    public sealed class LoadingScreen : Screen
+    {
+        
+    }
+}
---
+++ TopDownShooter.Unity/Assets/_Project/Develop/PlayForge Team/TopDownShooter/Runtime/UI/Screens/Screen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.TopDownShooter.Runtime.UI.Screens
+{
+    public abstract class Screen : MonoBehaviour
+    {
+        public void SetActive(bool value)
+        {
+            gameObject.SetActive(value);
+        }
+    }
+}
